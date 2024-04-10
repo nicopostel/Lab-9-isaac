ec1e48a9df4159955832d12983064e71dda3e8a1
isaacphilipose <isaacvp2@gmail.com>
2024-04-10 19:07:33 -0400

    added main function

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("hello")
+#isaac philipose
+
+def encoder(data):
+    pass
+
+
+
+def decoder(data):
+    pass
+
+
+while True:
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Exit")
+
+    choice = int(input("Please enter an option: "))
+    if choice == 1:
+        password = int(input("Please enter your password to encode: "))
+        encode = encoder(password)
+    if choice == 2:
+        decode = decoder(encode)
+        print(f"The encoded password is {encode}, and the original password is {decode}")
+    if choice == 3:
+        break
+
+
+
