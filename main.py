89af965f3c61ceece70c6870b34176c24ff19004
isaacphilipose <isaacvp2@gmail.com>
2024-04-10 19:11:28 -0400

    added main function

--- main.py
+++ main.py
@@ -8,23 +8,24 @@ def encoder(data):
 def decoder(data):
     pass
 
+def main()
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Exit")
 
-while True:
-    print("Menu")
-    print("-------------")
-    print("1. Encode")
-    print("2. Decode")
-    print("3. Exit")
+        choice = int(input("Please enter an option: "))
+        if choice == 1:
+            password = int(input("Please enter your password to encode: "))
+            encode = encoder(password)
+        if choice == 2:
+            decode = decoder(encode)
+            print(f"The encoded password is {encode}, and the original password is {decode}")
+        if choice == 3:
+            break
 
-    choice = int(input("Please enter an option: "))
-    if choice == 1:
-        password = int(input("Please enter your password to encode: "))
-        encode = encoder(password)
-    if choice == 2:
-        decode = decoder(encode)
-        print(f"The encoded password is {encode}, and the original password is {decode}")
-    if choice == 3:
-        break
-
+if __name__ == "__main__":
 
 
